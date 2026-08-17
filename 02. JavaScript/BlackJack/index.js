c865c5ec16e19c9575955b7c0b2be09bf9917c56
notGUMMY <rameshsuri21@gmail.com>
2026-08-17 23:47:29 +0530

    Blackjack

---
+++ 02. JavaScript/BlackJack/index.js
@@ -0,0 +1,19 @@
+let firstCard = 10
+let secondCard = 11
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+function startGame() {
+    if (sum < 21) {
+    message = "Do you want to draw a new card? 😊"
+    } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 😎"
+    hasBlackJack = true
+    } else {
+    message = "You're out of the game! 😢"
+    isAlive = false
+    }
+console.log(message)
+}
+
